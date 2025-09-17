a071a1c94c2fc9d90828e5f8cad3e61adefd09ed
al99nc <129080996+al99nc@users.noreply.github.com>
2025-09-17 10:25:48 +0300

    whipr

--- app/models/sellerProfile.py
+++ app/models/sellerProfile.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import enum
 import uuid
 from sqlalchemy import UUID, Boolean, DateTime, Enum, Text, Column, Integer, String, Float, ForeignKey, Mapped, func, relationship, mapped_column
-
+from app.models.user import User
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from app.db.database import Base
@@ -17,12 +17,10 @@ class SellerProfile(Base):
     __tablename__ = "sellerProfile"
     
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), )
-    
-    
-    
-    
-##  علاوي افتهم ال relationship هاي 
-## موجودة بال هستري بب بريف 
+    user_id = Column(ForeignKey("users.id", as_uuid=True), nullable=False)
+    user = relationship(User, uselist=False, backref="seller_profile")
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+
 #https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
-#your litterly my hero best best in the market i love uuuuuuuu so so much❤️❤️❤️❤️❤️
