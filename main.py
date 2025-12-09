4829a0219b5220964fce93570c368bb535c3d431
al99nc <jafarhamed25@gmail.com>
2025-12-09 22:06:02 +0300

    this5

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import app.models  # Ensure models are imported for SQLAlchemy to register them
 from app.routers import cart, product
 from app.routers import user
 from app.routers import sell
+import os
+print("=" * 50)
+print("ENVIRONMENT VARIABLES:")
+print(f"DATABASE_URL: {os.getenv('DATABASE_URL')}")
+print(f"PORT: {os.getenv('PORT')}")
+print("=" * 50)
 # all of this work is to just make these routers appear in the docs(swagger ui)
 app = FastAPI()
 app.include_router(user.router, tags=["Users"])
