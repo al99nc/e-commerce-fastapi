71b5c1f222e9a1a9f278c81673aba9ebc863e030
al99nc <jafarhamed25@gmail.com>
2025-12-09 23:53:02 +0300

    finael6

--- app/routers/home.py
+++ app/routers/home.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 router = APIRouter()
 @router.get("/")
 async def read_home():
-    return {"message": "Welcome to the Home Page!"}
+    return {"message": "hi there, you just hit the frist deployed project created by Ali Hamed!"}
