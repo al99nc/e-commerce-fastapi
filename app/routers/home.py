1907a1b668437d920572c942dd482695dd3e8863
al99nc <jaafarhamed25@gmail.com>
2025-12-13 00:58:22 +0300

    fineal6

--- app/routers/home.py
+++ app/routers/home.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.database import get_db
 from app.services.home import HomeServices
 
 router = APIRouter()
+
 @router.get("/")
-async def read_home():
-    homeservices = HomeServices()
+async def read_home(db: AsyncSession = Depends(get_db)):
+    homeservices = HomeServices(db)  # ✅ Pass db session
     return await homeservices.get_home_data()
-    
